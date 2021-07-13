5664160ae0d4deb1b71e53bf2b7c4ed7dc130144
ajeetkuma <ajeet.kumar6171@gmail.com>
2021-07-13 19:35:59 +0530

    sddsfds

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-app.use(express.static(__dirname + '../images'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
