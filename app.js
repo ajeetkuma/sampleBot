cb1b6a2cb6ebf3248497cf49be249817fb49cb39
ajeetkuma <ajeet.kumar6171@gmail.com>
2021-07-13 19:42:29 +0530

    sddsfds

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var path = require("path");
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.listen(3001);
+app.listen(process.env.PORT || 3001);
 console.log("Server running at Port 3001");
