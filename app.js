886739c2bf7f39e6fd66c01edfa042df5e9e82ba
FINEXUSAJEETKUMARCLS <ajeet.kumar@q2ebanking.com>
2021-07-13 18:47:05 +0530

    Added Files

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+app.use(express.static(__dirname + '../images'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.listen(3001);
+console.log("Server running at Port 3001");
